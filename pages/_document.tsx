598a4b0765de99c79e9d1c457dc97e2b679f465a
Emily Zhai <emily.zhai@convoy.com>
2020-10-17 00:16:48 -0700

    adding open graph meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,10 +6,13 @@ class Document extends NextDocument {
       <Html>
         <Head>
           <meta charSet="UTF-8" />
+          <meta property="og:title" content="Big Mack Truck" />
+          <meta property="og:description" content="If you know you know" />
+          <meta property="og:image" content="./screenshot.png" />
           <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>😛</text></svg>" />
-          <title>WAP</title>
         </Head>
         <body>
+          <title>WAP</title>
           <Main />
           <NextScript />
         </body>
